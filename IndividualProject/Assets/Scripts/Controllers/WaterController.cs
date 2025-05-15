bdddc56d4cc3323ede9e0f347be89f6a02210d76
jomeiii <136589834+jomeiii@users.noreply.github.com>
2025-05-15 20:57:20 +0500

    Add water

---
+++ IndividualProject/Assets/Scripts/Controllers/WaterController.cs
@@ -0,0 +1,29 @@
+using System;
+using Character.Player;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class WaterController : MonoBehaviour
+    {
+        [SerializeField] private TriggerController _triggerController;
+
+        private void OnEnable()
+        {
+            _triggerController.OnTriggerEnterEvent += TriggerEnter;
+        }
+
+        private void OnDisable()
+        {
+            _triggerController.OnTriggerEnterEvent -= TriggerEnter;
+        }
+
+        private void TriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out PlayerController playerController))
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
